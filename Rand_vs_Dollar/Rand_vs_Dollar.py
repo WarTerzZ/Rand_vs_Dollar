65cb5881485d1f8118675c0027cc16b2eed2a1ca
WarTerzZ <mmanzini@ska.ac.za>
2021-02-08 14:42:47 +0200

    Added 100 max incremented values

--- Rand_vs_Dollar/Rand_vs_Dollar.py
+++ Rand_vs_Dollar/Rand_vs_Dollar.py
@@ -4,7 +4,9 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from collections import namedtuple
+import math
+from decimal import *
 
 #col_types = [str, float, str, str, float, int]
 
@@ -15,10 +17,55 @@ import matplotlib.pyplot as plt
 #current_xchange = RnD_df["Ratio"] 
 #print(current_xchange)
 
-with open("./Rand vs. US Dollar.xlsx") as f:
-    f_csv = csv.reader(f, delimiter=",")
-    print(f_csv)
-    for rows in f_csv:
-        rows = tuple(f_csv[0:])
-        print(rows)
+def get_vals():
+    with open("./Rand vs. US Dollar.csv") as f:
+        f_csv = csv.reader(f, delimiter="\t")
+        headings = next(f_csv)
+        columns = next(f_csv)
+        # values = next(f_csv)
+        # values = []
+        values = next_value(f_csv)
+        Curr_Exch = []
+        inc_values = 0
+        values = next_value(f_csv)
+        for element in values:
+            num_of_elem = len(element)                      # 47 elements
+            for i in range(0, 100):
+                values = next_value(f_csv)
+                current_exchange = Curr_Exch.insert(i, element[35:43])
+                inc_values += i                             # Row 1 of Values
+                if inc_values == 100:
+                    inc_values = 0
+                    break
+            print(element)
+            print(Curr_Exch)
+            print(inc_values)
+
+def next_value(f_csv):
+    values = next(f_csv)
+    return values
+
+vals = get_vals()
+
+
+
+
+
+
+
+
+
+
+    #Row = namedtuple('Row', headings)
+    #for r in f_csv:
+    #    row = Row(*r)
+    #    print(row)
+
+    #for row in f_csv:
+    #    print(row)
+  #      print(column)
+        # Date = row[:1]
+        # print(Date)
+        # for line in row:
+        #    print(line)
 
